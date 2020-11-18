31538bf1eb608ee23404daf64369ebeab8b6f5b6
lph <liupeter012001@live.cn>
2020-11-18 19:29:19 +0800

    更新可能存在的逻辑错误

--- 特殊的后序遍历/Order.c
+++ 特殊的后序遍历/Order.c
@@ -12,21 +12,22 @@ void _PostOrderStack(BinNode* T)		//后序遍历栈版本子程序
                     LinkStack stack;
                     InitLinkStack(&stack);        //模拟栈的初始化操作
                     SpecialNode sn;     //新建结点
-                    sn.node = T;
-                    while (!isEmpty(stack) || sn.node != NULL)
+                    BinNode* p = T;
+                    while (!isEmpty(stack) || p != NULL)
                     {
-                              if (sn.node != NULL)
+                              if (p != NULL)
                               {
+                                        sn.node = p;
                                         sn.tag = L;
                                         Push_Stack(&stack, sn);
-                                        sn.node = sn.node->lchild;      //左子树
+                                        p = p->lchild;      //左子树
                               }
                               else
                               {
                                         Pop_Stack(&stack, &sn);       //获取栈顶元素
                                         if (sn.tag == L && sn.node->rchild != NULL)//左子树，还需要继续访问右子树且结点右子树不为空
                                         {
-                                                  sn.node = sn.node->rchild;                    //寻找右子树
+                                                  p = sn.node->rchild;                    //将右子树及其左子树一起入栈
                                                   sn.tag = R;         //修改状态标识
                                                   Push_Stack(&stack, sn);       //将父节点再次入栈
                                         }
